694758d9eb419c45f541be449844c286040e23f7
Davert <davert.php@resend.cc>
2014-02-12 05:05:21 +0200

    php 5.3 compatibility

--- tests/functional/StartDiscussionCept.php
+++ tests/functional/StartDiscussionCept.php
@@ -2,13 +2,13 @@
 $I = new TestGuy\UserSteps($scenario);
 $I->wantTo('start a discussion');
 $I->amAdmin();
-$I->haveCategory(['name' => 'Testing', 'slug' => 'test']);
+$I->haveCategory(array('name' => 'Testing', 'slug' => 'test'));
 $I->amOnPage('/');
 $I->see('Start a Discussion');
 $I->click('Discussions');
 $I->click('Start a Discussion');
 $I->see('Start a Discussion', 'h1');
-$I->seeRecord('Phosphorum\Models\Categories', ['name' => 'Testing']);
+$I->seeRecord('Phosphorum\Models\Categories', array('name' => 'Testing'));
 $I->fillField('#title', 'How can I write tests in Codeception');
 $I->fillField('#content', 'Is there any manual');
 $I->selectOption('#categoryId', 'Testing');
